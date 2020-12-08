fe349f943b7aaa40ca364995d5db0d9f96644d8f
Fergaletto <42767060+fergaletto@users.noreply.github.com>
2020-12-09 10:39:07 +1100

    Update runme.m

--- runme.m
+++ runme.m
@@ -5,11 +5,11 @@ close all
 clear all 
 
 % beginning.
-filename= 'cut.mkv';
+filename= 'highway.avi';
 startTime = 0; % number of seconds before start processing the video. 
-sensibilty = 0.1; % sensibility number, select 0.05 for more sensibility. 
-min_size = 1; % THe smallest bounding box displayed will be 20x20
-wsize = 16; % patch size to detect the change between frames. 
+sensibilty = 0.01; % sensibility number, select 0.05 for more sensibility. 
+min_size = 20; % THe smallest bounding box displayed will be 20x20
+wsize = 5; % patch size to detect the change between frames. 
 
 
 vidObj = VideoReader(filename);
@@ -19,8 +19,8 @@ fun = @(x) std(x.data(:)); % function for the blocproc
 
 % initialize the background. 
 back = 0
-for i = 1 : 20
-    back = back + 1/i *( rgb2gray(double(readFrame(vidObj))/255)-back);
+for i = 1 : 200
+    back = back + rgb2gray(double(readFrame(vidObj))/255)/200;
 end 
 
 % Read video frames until available
@@ -33,7 +33,7 @@ while hasFrame(vidObj)
     vidFrame = rgb2gray(Frame);
  
     % calculate the difference between the frame and background. 
-    dif=(100*(vidFrame-back).^2);
+    dif=((vidFrame-back).^2);
     
     % find the patches std to see where the changes are. 
     
@@ -41,15 +41,20 @@ while hasFrame(vidObj)
     result = (imresize((det), wsize, 'nearest' ));
 
         % Display the result 
-    figure(1)
-    imshow([vidFrame]);
-    
+
+
+
+     
     %plot bouding boxes
     T = graythresh(result); % find a threshold to binarize the std map
     T = imbinarize(result,max(T, sensibilty)); % binarize the std map
     
     %Background estimation for next frame.  
-    back = back + 1/100 *(vidFrame-back); % modified moving average MMA.
+    %back = (100*back+(vidFrame.*(1-T)+back.*T))/101;
+    %back = (100*back+vidFrame)/101;
+    back = back + 1/500.*(vidFrame-back);
+    figure(1)
+    imshow([vidFrame]);
     st = regionprops(T, 'BoundingBox' );
 
     hold on     
